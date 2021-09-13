c2b8df3680c9e929a6fece53d73c77c053e50be6
ViktoriiaPavlenko <vktr9999@ukr.net>
2021-09-13 21:47:50 +0300

    practice

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import validatePasword from './js/validate-password';
+// import apiService from './js/api-service';
+// import {
+//     fetchAllUsers,
+//     fetchUserById,
+//     updateUserById,
+//     x as value,
+//     y
+// } from './js/api-service';
+// import * as apiService from './js/api-service';
+import { addUser } from './js/api-service'
+
+addUser('mango')
+
+
+// console.log(validatePasword)
+// console.log(validatePasword('jhjjj'));
+// console.log(apiService);
+// console.log(fetchAllUsers);
+// console.log(fetchUserById);
+// console.log(updateUserById);
+// console.log(value);
+// console.log(y);
+// console.log(apiService);
+// console.log(validatePasword)
